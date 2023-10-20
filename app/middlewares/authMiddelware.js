3f257a726b1b8fdc130e7c2bbbb5a3fb515a85d8
SofiRoadToDev <sofi.workaffairs@gmail.com>
2023-10-20 11:08:26 -0300

    only indentations

--- app/middlewares/authMiddelware.js
+++ app/middlewares/authMiddelware.js
@@ -3,13 +3,9 @@ const jwt=require('jsonwebtoken')
 
 const SECRET='75dd04d5c47942d551b7387c5bb0a2249332add4530921de0dc0e61d13cb2598fd82370e1fa43ffd1c9d679fe933a8920a144202a8b2a1de01805ca6b7b0b779'
 const authMiddelware=async(req,res,next)=>{
-   
-    const {username}=req.body
     const headers=req.headers['authorization']   
-    if(!headers){
-        const token=await login(username)
-        req.headers['auth-token']=token
-        next()
+    if(!headers){        
+        res.status(401).json({err:'a token must be provided to access the resources'})
     }else{
         const token=headers.split(' ')[1]
         if(token){
@@ -18,8 +14,7 @@ const authMiddelware=async(req,res,next)=>{
                     res.status(403)
                 req.user=user
                 next()
-           })
-    
+           })   
         }
     }
     res.json({token:accessToken})
